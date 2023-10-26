94e42c512d5842c2b5df92d7a3bbac5c20e44cd1
cdswh <122427568+cdswh@users.noreply.github.com>
2023-10-25 21:14:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,4 @@
-import streamlit as st
-import ssl
-from PIL import Image
-from urllib.request import urlopen
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-custom_css = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                overflow: hidden;
-                background-image: url('https://i.postimg.cc/jqvTs0yP/CDS.png');
-                background-repeat: no-repeat;
-                background-size: cover;
-                background-position: center;
-            }
-            .stApp {
-                width: 750px;
-                height: 850px;
-                background-color: rgba(255, 255, 255, 0.85);
-                border: 8px solid #2470a0; 
-                border-radius: 20px;
-                padding: 10px; 
-                margin: 10px 0;
-                transform: translate(-50%, -50%);
-                left: 50%;
-                top: 50%;
-            }
-            h1 {
-                color: black;
-            }
-            .big-label {
-                color: black;
-                font-family: 'Roboto', sans-serif;
-                font-size: 2.5em;
-                font-weight: bold;
-            }
-            .output {
-                border-style: solid;
-                border-width: 3.5px;
-                border-radius: 10px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                color: black;
-                font-size: 2em;
-                font-weight: bold;
-            }
-            </style>
-            """
-st.markdown(custom_css, unsafe_allow_html=True)
-
-imageLOGO = Image.open(urlopen("https://i.ibb.co/WGjVK32/logopng.png"))
-st.image(imageLOGO)
-def calculate_weight(device):
+def calculate_labels_and_weight(device, qty, shipping_type):
     weights = {
         'Arrow - device only': 0.1,
         'Dagger Slim / Dagger Large': 0.5,
@@ -69,10 +9,7 @@ def calculate_weight(device):
         'EVO with harness': 0.5,
     }
     
-    return weights[device]
-
-def calculate_labels_and_weight(device, qty, shipping_type):
-    weight_per_device = calculate_weight(device)
+    weight_per_device = weights.get(device, 0)  # Get the weight for the specified device
     labels = []
 
     if shipping_type == "New Car":
@@ -80,7 +17,7 @@ def calculate_labels_and_weight(device, qty, shipping_type):
         remaining = qty % 50
 
         if total_boxes:
-            labels.append(f"({total_boxes}) Labels @ {(total_boxes * 50 * weight_per_device):.2f} lbs")
+            labels.append(f"({total_boxes}) Label{'s' if total_boxes > 1 else ''} @ {(total_boxes * 50 * weight_per_device):.2f} lbs each")
         if remaining:
             labels.append(f"(1) Label @ {(remaining * weight_per_device):.2f} lbs")
 
@@ -91,39 +28,14 @@ def calculate_labels_and_weight(device, qty, shipping_type):
         remaining_10 = remaining_20 % 10
 
         if boxes_20:
-            labels.append(f"({boxes_20}) Labels @ {(boxes_20 * 20 * weight_per_device):.2f} lbs")
+            labels.append(f"({boxes_20}) Label{'s' if boxes_20 > 1 else ''} @ {(boxes_20 * 20 * weight_per_device):.2f} lbs each")
         if boxes_10:
-            labels.append(f"({boxes_10}) Labels @ {(boxes_10 * 10 * weight_per_device):.2f} lbs")
+            labels.append(f"({boxes_10}) Label{'s' if boxes_10 > 1 else ''} @ {(boxes_10 * 10 * weight_per_device):.2f} lbs each")
         if remaining_10:
             labels.append(f"(1) Label @ {(remaining_10 * weight_per_device):.2f} lbs")
 
     return ' and '.join(labels)
 
-# Test with a quantity of 125
-result = calculate_labels_and_weight("YourDevice", 125, "New Car")
+# Test with a quantity of 125 of "Arrow with harness" for shipping type "New Car"
+result = calculate_labels_and_weight("Arrow with harness", 125, "New Car")
 print(result)
-
-
-def main():
-    st.title("RMA Weight Calculator")
-    
-    shipping_type = st.selectbox("Please select:", ['New Car', 'BHPH'])
-    
-    if shipping_type == "New Car":
-        device_options = ['Arrow - device only', 'Dagger Slim / Dagger Large', 'Harness Only', 'Arrow with harness']
-    else:
-        device_options = ['EVO - device only', 'REVO', 'Harness Only', 'EVO with harness']
-    device = st.selectbox("Select Item(s) Type:", device_options)
-    
-    qty = st.text_input("Enter the quantity of item(s) being returned:", value="1")
-    
-    if not qty.isdigit():
-        st.warning("Please enter a valid quantity.")
-        return
-    
-    labels = calculate_labels_and_weight(device, int(qty), shipping_type)
-    
-    st.markdown(f'<div class="output"> {labels}</div>', unsafe_allow_html=True)
-
-if __name__ == '__main__':
-    main()
